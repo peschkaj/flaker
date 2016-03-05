562b9b21251ed5f73e9ebabb5fc41c4549d92ded
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2016-03-05 14:53:40 -0800

    Added documentation and copyright notice.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+// Copyright (c) 2016 - Jeremiah Peschka
+//
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 pub mod flaker {
 
     extern crate time;
@@ -5,6 +20,7 @@ pub mod flaker {
     extern crate byteorder;
     use self::num::BigUint;
     use self::byteorder::{LittleEndian, WriteBytesExt};
+    
 
 
 
@@ -25,10 +41,21 @@ pub mod flaker {
     }
     
     impl Flaker {
+        /// Returns a new Flaker based on the specified identifier
+        ///
+        /// # Arguments
+        ///
+        /// * `identifier` - A 6 byte vec that provides some arbitrary identification.
         pub fn new_from_identifier(identifier: Vec<u8>) -> Flaker {
             Flaker::new(identifier, false)
         }
 
+        /// Returns a new Flaker based on the specified identifier
+        ///
+        /// # Arguments
+        ///
+        /// * `identifier` - A 6 byte vec that provides some arbitrary identification.
+        /// * `little_endian` - You know, just in case you have a little endian system. This is important for byte order when constructing the flake.
         pub fn new(identifier: Vec<u8>, little_endian: bool) -> Flaker {
             let mut l_identifier = identifier.clone();
             
@@ -46,6 +73,7 @@ pub mod flaker {
                 }
         }
 
+        /// Returns the current UNIX time in milliseconds
         fn current_time_in_ms() -> u64 {
             let now = time::now();
             let now_ts = now.to_timespec();
@@ -53,6 +81,11 @@ pub mod flaker {
             now_ts.sec as u64 + (now_ts.nsec as u64 / 1000 / 1000)
         }
         
+        /// Creates a new flake ID from the identifier, current time, and an internal counter.
+        /// Identifiers are generated as 128-bit numbers:
+        /// * 64-bit timestamp as milliseconds since the dawn of time (January 1, 1970)
+        /// * 48-bit worker identifier
+        /// * 16-bit sequence number that is incremented when more than one identifier is requested in the same millisecond and reset to 0 when the clock moves forward
         fn construct_id(&mut self) -> BigUint {
             // Create a new vec of bytes
             let mut bytes = Vec::new();
@@ -80,6 +113,7 @@ pub mod flaker {
     }
 
     impl HasFlakes for Flaker {
+        /// Update internal data structures.
         fn update(&mut self) -> Result<(), FlakeError> {
             let current_time_in_ms = Flaker::current_time_in_ms();
 
@@ -99,9 +133,8 @@ pub mod flaker {
             Ok(())
         }
 
-        // TODO signature needs to be changed to return a result
+        /// Generate a new ID 
         fn get_id(&mut self) -> Result<BigUint, FlakeError> {
-            // TODO check this for OK-ness
             let update_result = self.update();
             
             match update_result {
@@ -113,10 +146,13 @@ pub mod flaker {
 
     #[test]
     fn ids_change_over_time() {
+        use std::time::Duration;
+        use std::thread;
+        
         let mut f1 = Flaker::new_from_identifier(vec![0, 1, 2, 3, 4, 5]);
-        let id1 = f1.get_id();
-        std::thread::sleep(Duration::from_millis(50));
-        let id2 = f1.get_id();
+        let id1 = f1.get_id().unwrap();
+        thread::sleep(Duration::from_millis(50));
+        let id2 = f1.get_id().unwrap();
 
         println!("{} < {}", id1, id2);
 
@@ -127,8 +163,8 @@ pub mod flaker {
     fn ids_change_quickly() {
         let mut f1 = Flaker::new_from_identifier(vec![0, 1, 2, 3, 4, 5]);
 
-        let id3 = f1.get_id();
-        let id4 = f1.get_id();
+        let id3 = f1.get_id().unwrap();
+        let id4 = f1.get_id().unwrap();
 
         assert!(id3 < id4);
     }
